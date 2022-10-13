2c810e0569de44282fd78fd6693d909ecdcbade0
Michael Lesirge <michaellesirge@gmail.com>
2022-10-13 07:17:40 -0700

    rules of example problem. todo

---
+++ example_problem.py
@@ -0,0 +1,20 @@
+"""
+Department numbers:
+
+- Fire
+- Police
+- Sanatain
+
+Each assigned a number from 1 to 7
+All department # must be different
+Sum of all # must be 12
+Police must be even
+
+Display all valid department numbers permuations
+"""
+
+
+
+def department_numbers_permuations():
+    # for
+    pass
