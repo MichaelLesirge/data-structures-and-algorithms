c43e172dc0a8ca7ab74101175679df8e344563f5
Michael Lesirge <michaellesirge@gmail.com>
2022-10-14 21:05:51 -0700

    simple solution to department numbers problem

--- department_numbers_permuations.py
+++ department_numbers_permuations.py
@@ -1,5 +1,5 @@
 """
-Problem
+Problem from 
 
 Department numbers:
 
@@ -10,17 +10,41 @@ Department numbers:
 Each assigned a number from 1 to 7
 All department # must be different
 Sum of all # must be 12
-Police must be even
+Police cannot be odd
 
 Display all valid department numbers permuations
 """
+from collections import namedtuple
+from itertools import permutations
 
-import enum
+department_vals = namedtuple("departments", ("Fire", "Police", "Sanitation"))
 
-class departments(enum):
-    FIRE = 0
-    POLICE = 1
-    SANITATION = 2
+def department_numbers_permuations(min_num, max_num, required_sum = None, *, special_conditions = None):
+    # get all permutaions of vals
+    all_deparment_vals = permutations(range(min_num, max_num+1), len(department_vals._fields))
 
-def department_numbers_permuations():
-    pass
+    # convert to our named tuple
+    all_deparment_vals = map(lambda val: department_vals(*val), all_deparment_vals)
+
+    if required_sum is not None:
+        # just get conbantions where sum = the required sum
+        all_deparment_vals = filter(lambda val: sum(val) == required_sum, all_deparment_vals)
+
+    if special_conditions:
+        for special_condition in special_conditions:
+            all_deparment_vals = filter(special_condition, all_deparment_vals)
+
+    return all_deparment_vals
+
+def main() -> None:
+    MIN_NUM = 1
+    MAX_NUM = 7
+
+    REQUIRED_SUM = 12 
+
+    valid_department_numbers_permuations = department_numbers_permuations(MIN_NUM, MAX_NUM, REQUIRED_SUM, special_conditions=[lambda val: val.Police % 2 == 0])
+    for ans in valid_department_numbers_permuations:
+        print(ans)
+
+if __name__ == "__main__":
+    main()
