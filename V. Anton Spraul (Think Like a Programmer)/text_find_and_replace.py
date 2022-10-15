0fb3a497a6cb00e7572367a492fbc193d38af374
Michael Lesirge <michaellesirge@gmail.com>
2022-10-14 22:14:29 -0700

    solve find and replace in text problem

---
+++ V. Anton Spraul (Think Like a Programmer)/text_find_and_replace.py
@@ -0,0 +1,21 @@
+def find_and_replace(starting_text: str, old: str, new: str):
+    # no built in funtions/methods allowd
+    starting_text, old, new = list(starting_text), list(old), list(new)
+    
+    for start in range(len(starting_text)):
+        end = start + len(old)
+        if starting_text[start:end] == old:
+            starting_text[start:end] = new
+
+    return "".join(starting_text)
+
+def main():
+    starting_text = input("Enter starting text: ")
+
+    old = input("Enter text you want to replace: ")
+    new = input("Enter new text: ")
+
+    print(find_and_replace(starting_text, old, new))
+
+if __name__ == "__main__":
+    main()
