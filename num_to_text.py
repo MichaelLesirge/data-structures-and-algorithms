d29b5c452c4f1d65d911970308cd568fb6f63e48
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-21 22:39:22 -0700

    make work for all numbers up to 1 trillion

--- num_to_text.py
+++ num_to_text.py
@@ -11,7 +11,7 @@ tens = [
 ]
 
 large_units = [
-    "zero", "thousand", "million", "billion", "trillion",
+    "hundred" "thousand", "million", "billion", "trillion",
 ]
 
 def num_to_word(n):
@@ -19,21 +19,17 @@ def num_to_word(n):
         return special_units[n]
 
     if n < 100:
-        unit, extra = divmod(n, 10)
-        return tens[unit] + ("-" + num_to_word(extra) if extra != 0 else "")
-    
-    last = 1000
-    for i, unit_name in enumerate(large_units):
-        if (i == 0): continue
-        for j in range(3):
-            current = 1000 ** (i + j)
-            print((n,), (i, unit_name), divmod(n, last) if n <= current else (), (current, last))
-            if n <= current:
-                unit, extra = divmod(n, last)
-                return f"{num_to_word(unit)} {unit_name}{' ' + num_to_word(extra) if extra != 0 else ''}"
-            last = current
-    
-    raise Exception(f"Can not handle numbers larger than {current-1}")
+        value, extra = divmod(n, 10)
+        return tens[value] + ("-" + num_to_word(extra) if extra != 0 else "")
 
+    last = 100
+    current = 1
+    for word in large_units:
+        current *= 1000
+        if n < current:
+            value, extra = divmod(n, last)
+            return num_to_word(value) + " " + word + (" " + num_to_word(extra) if extra else "")
+        last = current
+    
 while True:
     print(num_to_word(int(input("Enter number: "))).title())
