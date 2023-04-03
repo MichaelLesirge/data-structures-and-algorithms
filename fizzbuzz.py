855d9336b0ccf44c877dff4204c996d3666b7c9f
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-02 17:23:28 -0700

    add silly oneliner version of fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 def main():
     print("Standard FizzBuzz")
-    fizzbuzz()
+    fizzbuzz_oneline()
 
     print("Custom Fizzbuzz")
     fizzbuzz(mapper = {2: "Even", 5: "Steven"})
@@ -12,11 +12,14 @@ def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     
     for num in range(1, n + 1):
         out = ""
-        for disable_num, message in mapper.items():
-            if num % disable_num == 0:
-                out += message
+        for divisor, word in mapper.items():
+            if num % divisor == 0:
+                out += word
         print(out or num)
 
+def fizzbuzz_oneline(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> None:
+    print("\n".join("".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num) for num in range(1, n + 1)))
+    
 
 if __name__ == "__main__":
     main()
