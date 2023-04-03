7aaeff4cbbf2be270f20c23d2cd7abe9469020fb
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-02 17:13:43 -0700

    formatting and fix type hint

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,12 +3,13 @@ def main():
     fizzbuzz()
 
     print("Custom Fizzbuzz")
-    fizzbuzz(mapper={2:"Even", 5:"Steven"})
+    fizzbuzz(mapper = {2: "Even", 5: "Steven"})
 
 
-def fizzbuzz(n=100, *, mapper: dict[int:str] = None) -> None:
+def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
+    
     for num in range(1, n + 1):
         out = ""
         for disable_num, message in mapper.items():
