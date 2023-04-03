27758ead34e06464b0cbeefe2680a9a7d28fc714
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-02 17:39:01 -0700

    change new fizzbuzz to be main one and reformat it

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,19 @@
 def main():
-    print("Standard FizzBuzz")
-    fizzbuzz_oneline()
+    print("Standard FizzBuzz:")
+    print(fizzbuzz())
 
-    print("Custom Fizzbuzz")
-    fizzbuzz(mapper = {2: "Even", 5: "Steven"})
+    print("Custom Fizzbuzz:")
+    print(fizzbuzz(mapper = {2: "Even", 5: "Steven"}))
 
 
-def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = None) -> None:
+
+def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
+    return "\n".join(
+        "".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num)
+        for num in range(1, n + 1))
+    
+# First one I made
+def fizzbuzz_first(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
     
@@ -16,9 +23,6 @@ def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = None) -> None:
             if num % divisor == 0:
                 out += word
         print(out or num)
-
-def fizzbuzz_oneline(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> None:
-    print("\n".join("".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num) for num in range(1, n + 1)))
     
 
 if __name__ == "__main__":
