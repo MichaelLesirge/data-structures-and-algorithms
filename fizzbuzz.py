c8f9454555f723f36f0b62ac6fc8b4714467b88a
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-13 15:03:33 -0700

    add my second edition

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,17 +2,33 @@ def main():
     print("Standard FizzBuzz:")
     print(fizzbuzz())
 
-    print("Custom Fizzbuzz:")
+    print("\nCustom Fizzbuzz:")
     print(fizzbuzz(mapper = {2: "Even", 5: "Steven"}))
+    
 
-
-
+# Final version, use list comp (may or may not be better depending on who you ask)
 def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
     return "\n".join(
         "".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num)
         for num in range(1, n + 1))
+
+
+# Second version, returns list instead of printing out immediately
+def fizzbuzz_second(n: int = 100, *, mapper: dict[int, str] = None) -> None:
+    if mapper is None:
+        mapper = {3: "Fizz", 5: "Buzz"}
     
-# First one I made
+    nums = []
+    for num in range(1, n + 1):
+        out = ""
+        for divisor, word in mapper.items():
+            if num % divisor == 0:
+                out += word
+        nums.append(out or str(num))
+    return "\n".join(nums)
+
+
+# First version
 def fizzbuzz_first(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
