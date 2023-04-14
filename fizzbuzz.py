b82a0c5df7e41240f0dfba1a995b2daf2ffa753a
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-14 05:31:58 -0700

    add newer version

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,26 @@
+from typing import Callable
+
+
 def main():
     print("Standard FizzBuzz:")
     print(fizzbuzz())
 
-    print("\nCustom Fizzbuzz:")
-    print(fizzbuzz(mapper = {2: "Even", 5: "Steven"}))
+    print("\nCustom Fizzbuzz with fizzbuzz_map={2: \"Even\", 5: \"Steven\"}:")
+    print(fizzbuzz(fizzbuzz_map={2: "Even", 5: "Steven"}))
     
+    print("\nCustom Fizzbuzz with compare_func=lambda num, compare: num % compare == 0 and num % 7 != 0)")
+    print(fizzbuzz(compare_func=lambda num, compare: num % compare == 0 and num % 7 != 0))
 
-# Final version, use list comp (may or may not be better depending on who you ask)
-def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
+
+# Final version, use passed in funtion to compare. Also don't set mutable object as default argument
+def fizzbuzz(upper_bound: int = 100, *, fizzbuzz_map: dict[int, str] = {3: "Fizz", 5: "Buzz"}, compare_func: Callable[[int, int], bool] = lambda num, compare: num % compare == 0) -> str:
+    return "\n".join(
+        "".join(word for compare_value, word in fizzbuzz_map.items() if compare_func(num, compare_value)) or str(num)
+        for num in range(1, upper_bound + 1))
+
+
+# Thrid version, use list comp (may or may not be better depending on who you ask)
+def fizzbuzz_thrid(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
     return "\n".join(
         "".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num)
         for num in range(1, n + 1))
@@ -17,7 +30,7 @@ def fizzbuzz(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -
 def fizzbuzz_second(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
-    
+
     nums = []
     for num in range(1, n + 1):
         out = ""
@@ -32,14 +45,14 @@ def fizzbuzz_second(n: int = 100, *, mapper: dict[int, str] = None) -> None:
 def fizzbuzz_first(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
-    
+
     for num in range(1, n + 1):
         out = ""
         for divisor, word in mapper.items():
             if num % divisor == 0:
                 out += word
         print(out or num)
-    
+
 
 if __name__ == "__main__":
     main()
