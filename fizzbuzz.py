2d6450e183e07f43ff9a5572cf0abf9cf9ecfe55
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-12-16 15:50:16 -0800

    add code golf fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+""""""
 
 def main():
     print("Standard FizzBuzz:")
@@ -8,26 +9,26 @@ def main():
     print("\nCustom Fizzbuzz with fizzbuzz_map={2: \"Even\", 5: \"Steven\"}:")
     print(fizzbuzz(fizzbuzz_map={2: "Even", 5: "Steven"}))
     
-    print("\nCustom Fizzbuzz with compare_func=lambda num, compare: num % compare == 0 and num % 7 != 0)")
-    print(fizzbuzz(compare_func=lambda num, compare: num % compare == 0 and num % 7 != 0))
 
-
-# Final version, use passed in funtion to compare.
+# Fourth and final version
+# Changes: use passed in function to compare, also better names
 def fizzbuzz(upper_bound: int = 100, *, fizzbuzz_map: dict[int, str] = {3: "Fizz", 5: "Buzz"}, compare_func: Callable[[int, int], bool] = lambda num, compare: num % compare == 0) -> str:
     return "\n".join(
         "".join(word for compare_value, word in fizzbuzz_map.items() if compare_func(num, compare_value)) or str(num)
         for num in range(1, upper_bound + 1))
 
 
-# Thrid version, use list comp (may or may not be better depending on who you ask)
-def fizzbuzz_thrid(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
+# Third version
+# Changes: use list comp (may or may not be better depending on who you ask)
+def fizzbuzz_v3(n: int = 100, *, mapper: dict[int, str] = {3: "Fizz", 5: "Buzz"}) -> str:
     return "\n".join(
         "".join(word for divisor, word in mapper.items() if num % divisor == 0) or str(num)
         for num in range(1, n + 1))
 
 
-# Second version, returns list instead of printing out immediately
-def fizzbuzz_second(n: int = 100, *, mapper: dict[int, str] = None) -> None:
+# Second version
+# Changes: returns list instead of printing out immediately
+def fizzbuzz_v2(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
 
@@ -42,7 +43,8 @@ def fizzbuzz_second(n: int = 100, *, mapper: dict[int, str] = None) -> None:
 
 
 # First version
-def fizzbuzz_first(n: int = 100, *, mapper: dict[int, str] = None) -> None:
+# Fizzbuzz with mapper
+def fizzbuzz_v1(n: int = 100, *, mapper: dict[int, str] = None) -> None:
     if mapper is None:
         mapper = {3: "Fizz", 5: "Buzz"}
 
@@ -54,5 +56,13 @@ def fizzbuzz_first(n: int = 100, *, mapper: dict[int, str] = None) -> None:
         print(out or num)
 
 
+# My Code Golf FizzBuzz Function. 
+def f():[print('Fizz'[i%3*4:]+'Buzz'[i%5*4:]or i) for i in range(1,101)]
+
+# Fizzbuzz in just 62 charters. Only 3 more charters than the shortest one possible fizzbuzz in python3 (at least from what I found)
+# for i in range(1,101):print('Fizz'[i%3*4:]+'Buzz'[i%5*4:]or i)
+
 if __name__ == "__main__":
-    main()
+    for i in range(100):print(i%3//2*"Fizz"+i%5//4*"Buzz"or-~i)
+    # f()
+    # main()
